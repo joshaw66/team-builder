932bec61b6999e82e250df53d29c828bc13aabb2
Joshua Shaw <joshua@Joshuas-MacBook-Pro.local>
2020-03-19 13:16:56 -0600

    making progress w/ Form.js and TeamMembers.js

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,1 +1,14 @@
+import React, { useState } from 'react';
 
+const MemberForm = props => {
+  const [member, setMember] = useState ({
+    id: Date.now(),
+    name: "",
+    email: "",
+    position: ""
+  });
+
+
+}
+
+export default MemberForm;
